ae22b47876042708e322fb159d5d9b6cfea2dbed
Janith Gamage <janithjayasankha@gmail.com>
2023-05-16 19:12:09 +0530

    Init

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+    return (
+        <button className="bg-noonYellow-500 hover:bg-noonYellow-700 relative inline-flex shrink-0 items-center justify-center overflow-hidden text-center text-xs font-medium tracking-wider outline-none transition-all sm:text-sm px-7 sm:px-9 h-11 sm:h-13">
+            Button
+        </button>
+    );
+};
